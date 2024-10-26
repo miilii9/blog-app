3e3466bf5fe50be8c2cb5478a20ff2a90343e3c0
the joker <milad.ek99@yahoo.com>
2024-10-26 14:23:44 +0330

    refactor

--- app/(user)/blogs/[slug]/page.tsx
+++ app/(user)/blogs/[slug]/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
-const DynamicBlogDetail = dynamic(() => import("./components/blog-detail"));
+const BlogDetail = dynamic(() => import("./components/blog-detail"));
 
 export const metadata: Metadata = {
   title: "بلاگ",
   description: "جزییات بلاگ",
 };
 const page = () => {
-  return <DynamicBlogDetail />;
+  return <BlogDetail />;
 };
 
 export default page;
